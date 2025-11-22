2c14dafc05027edfc1b70a45adf099df0ed31e3e
Mghetu <mihai.ghetu@gmail.com>
2025-11-22 22:34:28 +0200

    Revert MJML export and conversion

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,4 @@ export default defineConfig({
   plugins: [react()],
   base: '/mjml-react-test/',        // must match your repo name
   build: { outDir: 'dist', target: 'esnext' },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
 });
